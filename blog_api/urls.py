15635d80f6967d1fc0811cb03a361999f0ae938b
Felipe Campelo <felipe@thearch.tech>
2023-03-17 23:13:11 -0300

    Changing project name, folder name, and adjusting the things to run the server correctly in dev env

--- blog_api/urls.py
+++ blog_api/urls.py
@@ -1,4 +1,4 @@
-"""blogging_platform URL Configuration
+"""blog_api URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.1/topics/http/urls/
@@ -11,7 +11,7 @@ Class-based views
     2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
 Including another URLconf
     1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+    2. Add a URL to urlpatterns:  path('api/', include('blog.urls'))
 """
 from django.contrib import admin
 from django.urls import path, include
@@ -20,5 +20,5 @@ from rest_framework.documentation import include_docs_urls
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('blog.urls')),
-    path('api/docs/', include_docs_urls(title='Blogging Platform API Documentation'))
+    path('api/docs/', include_docs_urls(title='Simple Blog API Documentation'))
 ]
